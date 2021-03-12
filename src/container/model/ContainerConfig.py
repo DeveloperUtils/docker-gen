517f2f9658b00b8e86676ba8cdf79620746a6a7d
Christoph Graupner <ch.graupner@workingdeveloper.net>
2021-03-12 12:20:16 +0100

    move constants to Config.py

--- src/container/model/ContainerConfig.py
+++ src/container/model/ContainerConfig.py
@@ -1,12 +1,8 @@
 import logging
 from typing import Optional
 
+from src.Config import KEY_ENABLED, KEY_OUTBOUND, KEY_URL
 from src.Utils import is_true
-
-KEY_PREFIX = "com.github.developerutils.docker-gen"
-KEY_ENABLED = KEY_PREFIX + ".enabled"
-KEY_OUTBOUND = KEY_PREFIX + ".outbound"
-KEY_URL = KEY_PREFIX + ".url"
 
 logger = logging.getLogger(__name__)
 
