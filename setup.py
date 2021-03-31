2031480c15da35ae14c599c19d2c5e948dfc9fe7
Christoph Graupner <ch.graupner@workingdeveloper.net>
2021-03-31 10:20:54 +0200

    support ssl with custom ssl cert path

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='docker-gen-nxt',
-    version='0.1.0',
+    version='0.2.0',
     description="Says hello",
     author="Christoph Graupner",
     author_email='ch.graupner@workingdeveloper.net',
