e315a379dd420535d8b301ed72d4179adc612c54
Christoph Graupner <ch.graupner@workingdeveloper.net>
2021-04-28 09:57:19 +0200

    remove unused file

--- src/render/OutputRenderer.py
+++
@@ -1,7 +0,0 @@
-class OutputRenderer:
-    def __init__(self):
-        pass
-
-    def render(self, template: str, destination: str):
-        template = Template('Hello {{ name }}!')
-        template.render(name='John Doe')
